71c93ad3db8832b9d15f69dda9e292b8fdc200be
Mohamed Rezk <mohamed.rezk@orbyta.it>
2022-02-18 15:56:34 +0100

    first commit

---
+++ src/main/java/com/fakegram/microservices/gateway/FakegramGatewayMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fakegram.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakegramGatewayMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakegramGatewayMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fakegram/microservices/gateway/FakegramGatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakegram.microservices.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakegramGatewayMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
